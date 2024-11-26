2ea9266ab76688b68c49ad79c3826394f0d1ca7d
Nilesh Awachat <nileshawachat11@gmail.com>
2024-11-26 20:48:44 +0530

    dispalyItems using usestate hook

---
+++ src/UseState/DisplayItems.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+
+function DisplayItems() {
+
+    // State to track the input field value
+    const [inputValue, setInputValue] = useState("");
+
+    // State to track the list of items
+    const [items, setItems] = useState([]);
+
+    const handleRemoveItem = (indexToRemove) => {
+        setItems((prevItems) => prevItems.filter((_, index) => index !== indexToRemove));
+    };
+
+    return (
+        <>
+
+            <div>
+
+                <input
+                    type="text"
+                    placeholder="Type an item"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)} />
+
+
+
+
+                <button onClick={() => {
+                    if (inputValue.trim() !== "") {
+                        setItems([...items, inputValue])
+                        setInputValue('')
+                    }
+                }}>ADD</button>
+
+                <div>
+                    <ul>
+                        {items.map((item, index) => (
+                            <li key={index}>{item}
+                            
+                                
+                                 <button
+                                    onClick={() => {
+                                        const updatedItems = items.filter((_, i) => i !== index);
+                                        setItems(updatedItems);
+                                    }}
+                                >
+                                    Remove
+                                </button>
+                                
+                                </li>
+                        ))}
+                    </ul>
+                </div>
+
+
+            </div>
+
+        </>
+    )
+}
+
+export default DisplayItems
